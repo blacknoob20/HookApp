a5a81934a10cc774413578b99d7a4df2a8dc3bcf
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-07-12 16:15:12 -0500

    Hook useState()

---
+++ src/components/01-useState/CounterApp.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+import './counter.css';
+
+export const CounterApp = () => {
+    // Uso de useState con objetos
+    const [state, setState] = useState({
+        counter1: 10,
+        counter2: 20
+    });
+
+    const {counter1, counter2} = state;
+
+    return (
+        <>
+            <h1>Counter 1 { counter1 }</h1>
+            <h1>Counter 2 { counter2 }</h1>
+            <hr/>
+            <button
+                className="btn btn-primary"
+                onClick={() => {
+                    setState({
+                        ...state,
+                        counter1: (counter1 +1)
+                    });
+                }}
+            >+1</button>
+        </>
+    )
+
+    // Uso de useState con variables
+    // const [counter, setCounter] = useState(10);
+
+    // return (
+    //     <>
+    //         <h1>Counter {counter}</h1>
+    //         <hr/>
+    //         <button
+    //             className="btn btn-primary"
+    //             onClick={() => {
+    //                 setCounter(counter +1);
+    //             }}
+    //         >+1</button>
+    //     </>
+    // )
+}
