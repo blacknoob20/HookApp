3d3f22ca4b3a4018bf8caba7e1073806c4e1560c
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-07-13 11:50:34 -0500

    Hook useEffect()

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+
+import './effect.css';
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    });
+
+    const {name, email} = formState;
+
+    useEffect(() =>{
+        console.log('hey!');
+    },[]);
+
+    useEffect(() =>{
+        console.log('formState cambió');
+    },[formState]);
+
+    useEffect(() =>{
+        console.log('email cambió');
+    },[email]);
+
+    const handleInputChange = ({target}) => {
+        // console.log(e.target);
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        })
+    }
+
+    return (
+        <div>
+            <h1>useEffect</h1>
+            <hr/>
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Tu nombre"
+                    autoComplete="off"
+                    value={name}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="correo@gmail.com"
+                    autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
+                />
+            </div>
+        </div>
+    )
+}
