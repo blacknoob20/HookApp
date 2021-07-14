cbb6e7358241eb3385b64bbb205625c9bef55c7e
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-07-14 15:11:43 -0500

    Hook useRef(), caso de uso real

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const useFetch = (url) => {
     const req = {data:null,loading:true,error:null};
+    const isMounted = useRef(true);
+    // let isMounted = true;
     const [state, setState] = useState(req);
+
+    useEffect(() => {
+        return () =>{
+            isMounted.current = false;
+            // isMounted = false;
+        }
+    },[]);
 
     useEffect(() => {
         setState(req);
@@ -10,11 +19,16 @@ export const useFetch = (url) => {
         fetch(url)
             .then(resp => resp.json())
             .then(data => {
-                setState({
-                    loading: false,
-                    error: null,
-                    data
-                });
+                setTimeout(() => {
+                    if (isMounted.current) {
+                    // if (isMounted) {
+                        setState({
+                            loading: false,
+                            error: null,
+                            data
+                        });
+                    } else { console.log('No se llamó el quote'); }
+                }, 4000);
             });
     }, [url]);
 
