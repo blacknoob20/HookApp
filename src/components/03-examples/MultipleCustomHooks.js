1595eded2fafc4627aef723c54a9ca3ba8509bbc
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-07-13 23:49:38 -0500

    useFetch + useCounter | Primera parte

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -4,13 +4,27 @@ import { useFetch } from '../../hooks/useFetch';
 import '../02-useEffect/effect.css';
 
 export const MultipleCustomHooks = () => {
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+    const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+    const { author, quote } = !!data && data[0];
     
-    console.log(state);
-
     return (
         <div>
-            <h1>Multiple Custom Hooks</h1>
+            <h1>BreakingBad Quotes</h1>
+            <hr/>
+            {
+                loading
+                ?
+                    (
+                        <div className="alert alert-info text-center">Loading...</div>
+                    )
+                :
+                    (
+                        <blockquote className="blockquote text-end">
+                            <p className="mb-0">{quote}</p><br/>
+                            <footer className="fs-6 text blockquote-footer">{author}</footer>
+                        </blockquote>
+                    )
+            }
         </div>
     )
 }
