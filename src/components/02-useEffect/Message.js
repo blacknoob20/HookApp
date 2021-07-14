59c72a38a2371f8c081e563f2fb9b6490fae201f
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-07-13 22:30:14 -0500

    Hook useEffect() precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,16 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
+    const [coords, setCoords] = useState({x:0,y:0});
+    const {x,y} = coords
+
     useEffect(() => {
-        console.log('componente montado')
+        // console.log('componente montado')
+        // Para remover el listener hay que poner el callback en una variable
+        const mouseMove = (e) => {
+            const coords = {x: e.x, y: e.y};
+            setCoords(coords);
+        }
+
+        window.addEventListener('mousemove',mouseMove);
+        
         return () => {
-            console.log('componente desmontado')
+            // console.log('componente desmontado')
+            window.removeEventListener('mousemove',mouseMove);
         }
     }, []);
 
     return (
         <>
             <h3>Eres genial...!</h3>
+            <p>x:{x} y:{y}</p>
         </>
     )
 }
