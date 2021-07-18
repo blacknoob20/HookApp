277bd0a7a1f86456f620108883d4380259198c5c
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-07-18 01:47:24 -0500

    Hook useLayoutEffect()

---
+++ src/components/05-useLayoutEffect/Layout.js
@@ -0,0 +1,29 @@
+import React, { useLayoutEffect, useRef } from 'react';
+import { useCounter } from '../../hooks/useCounter';
+import { useFetch } from '../../hooks/useFetch';
+
+import './layout.css';
+
+export const Layout = () => {
+    const {counter, increment} = useCounter(1);
+
+    const { data } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    const { quote } = !!data && data[0];
+
+    const pTag = useRef()
+
+    useLayoutEffect(() => {
+        console.log(pTag.current.getBoundingClientRect());
+    },[quote]);
+
+    return (
+        <div>
+            <h1>Layout Effect</h1>
+            <hr/>
+            <blockquote className="blockquote text-end">
+                <p className="mb-0" ref={pTag}>{quote}</p><br/>
+            </blockquote>
+            <button className="btn btn-primary" onClick={ increment }>Sig. Quote {counter}</button>
+        </div>
+    )
+}
