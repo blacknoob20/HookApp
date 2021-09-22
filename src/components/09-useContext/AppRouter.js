c21d98ff173ef5eb308c6f1a6cb9369a05a40831
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-09-21 20:49:04 -0500

    Hook useContex | Component NavLink

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { AboutPage } from './AboutPage';
 import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
@@ -14,7 +14,8 @@ export const AppRouter = () => {
                     <Route exact path="/about" component={AboutPage} />
                     <Route exact path="/login" component={LoginPage} />
                     <Route exact path="/" component={HomePage} />
-                    <Route component={HomePage} />
+                    <Redirect to="/" />
+                    {/* <Route component={HomePage} /> */}
                 </Switch>
             </div>
         </Router>
--- src/components/09-useContext/NavBar.js
+++ src/components/09-useContext/NavBar.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
     return (
-        <nav>
-            <ul>
-                <li><Link to="./">Home</Link></li>
-                <li><Link to="./about">About</Link></li>
-                <li><Link to="./login">Login</Link></li>
-            </ul>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <NavLink className="navbar-brand" to="./">useContext</NavLink>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <NavLink exact activeClassName="active" className="nav-item nav-link" to="./">Home</NavLink>
+                    <NavLink exact activeClassName="active" className="nav-item nav-link" to="./about">About</NavLink>
+                    <NavLink exact activeClassName="active" className="nav-item nav-link" to="./login">Login</NavLink>
+                </div>
+            </div>
         </nav>
     )
 }
