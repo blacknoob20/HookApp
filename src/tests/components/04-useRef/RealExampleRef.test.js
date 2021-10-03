d6e412cbb3772913f58f7b2f2af5bf2ebac6e374
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-10-03 15:11:22 -0500

    Pruebas sobre CustomHooks | Interacciones con el useState()

---
+++ src/tests/components/04-useRef/RealExampleRef.test.js
@@ -0,0 +1,18 @@
+import { shallow } from 'enzyme';
+import { RealExampleRef } from '../../../components/04-useRef/RealExampleRef';
+
+describe('Pruebas en <RealExampleRef />', () => {
+    const wrapper = shallow(<RealExampleRef />);
+
+    test('Debe mostrarse correctamente', () => {
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('MultipleCustomHooks').exists()).toBe(false);
+    });
+
+    test('Debe mostrar el componente <MultipleCustomHooks/>', () => {
+        wrapper.find('button').simulate('click');
+        expect(wrapper.find('MultipleCustomHooks').exists()).toBe(true);
+    });
+
+});
